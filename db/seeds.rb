320efb3a086ff54f3e6cbe6f7b5a2f68388ebe76
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-08 13:33:55 +0530

    faker gem - seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do |i|
+  Book.create(
+    book_name: Faker::Book.title,
+    genre: Faker::Book.genre,
+    author_name: Faker::Book.author,
+    summary: Faker::Quote.famous_last_words + Faker::Quote.famous_last_words,
+    price: Faker::Number.decimal(l_digits: 3, r_digits: 3),
+    published_date: Faker::Date.between(from: "2015-12-20", to: "2022-03-28"),
+  )
+  puts "Book #{i} Seeded"
+end
