78cac82e89027ba34cc11c0ec38a955f24010a23
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-08 13:30:20 +0530

    rails 7 app initialized

---
+++ db/migrate/20220805060313_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :book_name
+      t.string :genre
+      t.string :author_name
+      t.text :summary
+      t.float :price
+      t.date :published_date
+
+      t.timestamps
+    end
+  end
+end
